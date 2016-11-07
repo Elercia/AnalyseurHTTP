a422a53c28e88a3d0a4973500910ed9fd7da8837
Vincent Rabier <rabierv@hotmail.fr>
2016-11-07 10:53:14 +0100

    ajout début fonction de récupération entre serveur -- client

--- src/model/proxy/ProxyHTTP.java
+++ src/model/proxy/ProxyHTTP.java
@@ -4,13 +4,13 @@ import java.net.*;
 import java.io.*;
 
 public class ProxyHTTP extends Thread {
-    private Socket socket = null;
+    private Socket socketClient = null;
     private static final int BUFFER_SIZE = 32768;
 
 
     public ProxyHTTP(Socket socket) {
         super("ProxyHTTP");
-        this.socket = socket;
+        this.socketClient = socket;
     }
 
     public void run() {
@@ -20,8 +20,8 @@ public class ProxyHTTP extends Thread {
         //send response to user
 
         try {
-            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream outClient = new DataOutputStream(socketClient.getOutputStream());
+            BufferedReader inClient = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
 
             String inputLine, outputLine;
             int cnt = 0;
@@ -31,7 +31,7 @@ public class ProxyHTTP extends Thread {
             //Début de la récupération de la demande du client
             ///////////////////////////////////
             String UserRequest = "";
-            while ((inputLine = in.readLine()) != null) {
+            while ((inputLine = inClient.readLine()) != null) {
                 try {
                     UserRequest += inputLine+"\n";
                 } catch (Exception e) {
@@ -46,10 +46,31 @@ public class ProxyHTTP extends Thread {
             //Fin de la récupération de la demande du client
             ///////////////////////////////////
 
+
             ///////////////////////////////////
             //Envoie de la requete au serveur + récupération de la réponse
             ///////////////////////////////////
 
+            try {
+                URL u = new URL("http://www.google.com");
+                Proxy pr = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxyetu.iut-nantes.univ-nantes.prive", 3128));
+
+                HttpURLConnection uc = (HttpURLConnection) u.openConnection(pr);
+                uc.connect();
+                System.out.println(uc);
+
+                InputStream i = uc.getInputStream();
+                InputStreamReader isr = new InputStreamReader(i);
+                BufferedReader br = new BufferedReader(isr);
+                String line;
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                }
+                System.out.println(uc.getResponseMessage());
+            }catch(IOException e){
+                System.out.println("Probleme "+e.getMessage());
+                e.printStackTrace();
+            }
 
             ///////////////////////////////////
             //Fin Envoie de la requete au serveur + récupération de la réponse
@@ -62,15 +83,7 @@ public class ProxyHTTP extends Thread {
                 //Envoie de la réponse au client
                 ///////////////////////////////////
 
-                // byte by[] = new byte[ BUFFER_SIZE ];
-                // int index = is.read( by, 0, BUFFER_SIZE );
-                // while ( index != -1 )
-                // {
-                //   out.write( by, 0, index );
-                //   index = is.read( by, 0, BUFFER_SIZE );
-                // }
-                // //out.writeBytes("<h1>TEST</h1>");
-                //out.flush();
+
 
 
                 ///////////////////////////////////
@@ -82,21 +95,21 @@ public class ProxyHTTP extends Thread {
                 //e.printStackTrace();
                 //encountered error - just send nothing back, so
                 //processing can continue
-                out.writeBytes("");
+                outClient.writeBytes("");
             }
 
             //close out all resources
             if (rd != null) {
                 rd.close();
             }
-            if (out != null) {
-                out.close();
+            if (outClient != null) {
+                outClient.close();
             }
-            if (in != null) {
-                in.close();
+            if (inClient != null) {
+                inClient.close();
             }
-            if (socket != null) {
-                socket.close();
+            if (socketClient != null) {
+                socketClient.close();
             }
 
         }catch(IOException e){
