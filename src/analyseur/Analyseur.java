11d1856aada23bae9954a7d1054a59b4d23cc413
Loïc <lolo.49400@hotmail.fr>
2016-11-07 10:05:47 +0100

    Ajout des méthodes de Analyseur

--- src/analyseur/Analyseur.java
+++ src/analyseur/Analyseur.java
@@ -2,4 +2,27 @@ package analyseur;
 
 public class Analyseur {
 
+  public Analyseur(){
+    //TODO
+  }
+
+  public void debutEcoute(int port){
+    //TODO
+  }
+
+  public void finEcoute(){
+    //TODO
+  }
+
+  public void traite(String requete){
+    //TODO
+  }
+
+  public String recuperationDonnees(String donnees){
+    //TODO
+  }
+
+  public String traiteAffichage(String s){
+    //TODO
+  }
 }
