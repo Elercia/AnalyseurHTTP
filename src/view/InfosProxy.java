8fa40e6bc6b2a44eac8f671e94e2a58a9cc22718
Jules LE BRIS <E155251B@U-INF-J-C001-20.iut-nantes.univ-nantes.prive>
2017-01-04 13:34:49 +0100

    marche pas

--- src/view/InfosProxy.java
+++ src/view/InfosProxy.java
@@ -4,10 +4,59 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Map;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.List;
+import com.btr.proxy.search.ProxySearch;
 
-public class TEST_JULES {
-    public static void main(String[] args) throws IOException {
-        /*Process p = Runtime.getRuntime().exec("echo $HTTP_PROXY");
+public class InfosProxy {
+        private String hote;
+        private int port;
+
+        public InfosProxy() {
+            try {
+                System.setProperty("java.net.useSystemProxies", "true");
+                ProxySearch ps = ProxySearch.getDefaultProxySearch();
+                ps.setPacCacheSettings(32, 1000 * 60 * 5);
+                List<Proxy> l = ps.getProxySelector().select(new URI("http://www.google.com/")); // Pour pouvoir effectuer une requête d'essai
+
+                for (Iterator<Proxy> iter = l.iterator(); iter.hasNext(); ) {
+                    Proxy proxy = (Proxy) iter.next();
+                    InetSocketAddress addr = (InetSocketAddress) proxy.address();
+
+                    if (addr == null) {
+                        this.hote = "null";
+                    } else {
+                        this.hote = proxy.type().toString().toLowerCase() + "://" + addr.getHostName();
+                        this.port = addr.getPort();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        // https://code.google.com/archive/p/proxy-vole/downloads
+        public String getHote() {
+            return this.hote;
+        }
+
+        public int getPort() {
+            return this.port;
+        }
+
+        public static void main(String args[]) {
+            InfosProxy i = new InfosProxy();
+            System.out.println(i.getHote() + ":" + i.getPort());
+        }
+
+}
+
+
+    /*public static void main(String[] args) throws IOException {
+        Process p = Runtime.getRuntime().exec("echo $HTTP_PROXY");
 
         // read the standard output of the command
         BufferedReader stdInput = new BufferedReader(new InputStreamReader(p
@@ -50,11 +99,11 @@ public class TEST_JULES {
         Runtime runtim = Runtime.getRuntime();
         BufferedReader reade = new BufferedReader(new InputStreamReader(runtim.exec("echo HTTP_PROXY").getInputStream()));
         String ress = reade.readLine();
-        System.out.println("6 "+ress);*/
+        System.out.println("6 "+ress);
 
         Map<String, String> maMap = System.getenv();
         System.out.println(maMap.toString());
         System.out.println(maMap.containsKey("HTTP_PROXY"));
         System.out.println(maMap.containsKey("$HTTP_PROXY"));
     }
-}
+}*/
